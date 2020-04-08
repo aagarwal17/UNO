1f8006ef4f73175ed268f72e31740727c4e9f183
Arun Agarwal <tuk82261@temple.edu>
2020-04-08 13:30:20 -0400

    4/8/20

--- My UNO Project/src/CardPool.java
+++ My UNO Project/src/CardPool.java
@@ -3,19 +3,19 @@ import java.util.Collections;
 
 public class CardPool
 {
-	ArrayList< UnoCard > deck = new ArrayList< UnoCard >;
-	ArrayList< UnoCard > discard = new ArrayList< UnoCard >;
-	ArrayList< UnoCard > hand = new ArrayList< UnoCard >;
+	ArrayList< UnoCard > deck = new ArrayList< UnoCard >();
+	ArrayList< UnoCard > discard = new ArrayList< UnoCard >();
+	ArrayList< UnoCard > hand = new ArrayList< UnoCard >();
+	
+	final private int standardFaces = 13;
+	final private int standardColors = 4;
 	
 	private int size = 108;
 	
 	public void generateDeck()
 	{	
-		private int standardColors = 4;
-		private int colorCounter = 1;
-		
-		private int standardFaces = 13;
-		private int faceCounter = 1;
+		int colorCounter = 1;
+		int faceCounter = 1;
 	
 		for ( UnoCard.Color color : UnoCard.Color.values() )
 		{
@@ -23,23 +23,23 @@ public class CardPool
 				break;
 			else 
 			{
-				deck.add( UnoCard(color, UnoCard.Face.ZERO) )
+				deck.add( new UnoCard(color, UnoCard.Face.ZERO) );
 				
 				for( UnoCard.Face face : UnoCard.Face.values() ) 
 				{
 					if( faceCounter == standardFaces )
 						break;
 					else
-						deck.add( UnoCard(color, face) );
+						deck.add( new UnoCard(color, face) );
 				}
 			}
 		}
 		
 		for( int i = 0; i < 4; i++ )
-			deck.add( UnoCard(UnoCard.Color.WILD, UnoCard.Face.BLANK) );
+			deck.add( new UnoCard(UnoCard.Color.WILD, UnoCard.Face.BLANK) );
 			
 		for( int i = 0; i < 4; i++ )
-			deck.add( UnoCard(UnoCard.Color.WILD, UnoCard.Face.DRAW4) );
+			deck.add( new UnoCard(UnoCard.Color.WILD, UnoCard.Face.DRAW4) );
 		
 		shuffleDeck();
 		
